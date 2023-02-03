7839a91b044546f5d4d22e15f5e494a86a32c165
Abdulai Musah <abdulaimusah31@gmail.com>
2023-02-03 11:19:29 +0000

    rest

--- src/myheader.js
+++ src/myheader.js
@@ -5,14 +5,14 @@ const MyHeader = () => {
 
     return (
         <header className="bg-white flex
-        shadow-sm shadow-gray-300 h-14 
+        shadow-sm shadow-gray-300 h-20 
         font-medium text-lg 
         items-center justify-center
         flex-shrink-0 
         "  >
-            <div className="h-8 mr-2" >
+            <div className="h-16 mr-2" >
                 <img src="/hlogo.PNG" 
-                alt="Google Pay"
+                alt="happn"
                 className="max-h-full w-auto" />
             </div>
             <h1>
